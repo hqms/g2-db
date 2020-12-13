85236b27696299e7b045c3833a0323a364b20d52
HQM <hakim@femaledaily.com>
2020-12-14 05:33:19 +0700

    initiate demo mongo app

---
+++ demongo/src/main/java/com/hqm/demongo/DemongoApplication.java
@@ -0,0 +1,13 @@
+package com.hqm.demongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemongoApplication.class, args);
+	}
+
+}
