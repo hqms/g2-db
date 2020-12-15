79a397dfef2af948b56dee3ebeb13dff5db48efd
HQM <hakim@femaledaily.com>
2020-12-16 06:40:41 +0700

    sqlite support

--- api-demo/src/test/java/com/hqm/demoapi/DemoApiApplicationTests.java
+++ api-demo/src/test/java/com/hqm/demoapi/DemoApiApplicationTests.java
@@ -1,13 +1,16 @@
 package com.hqm.demoapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class DemoApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(1,1);
 	}
 
 }
